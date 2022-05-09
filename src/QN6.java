fba03d1bb6aed53bd1e706201dc7abd6c3b450d6
Anil Khatiwada <anilkhatiwada@gmail.com>
2022-05-09 21:44:47 +0545

    edited

--- src/QN6.java
+++ src/QN6.java
@@ -1,12 +1,26 @@
 public class QN6 {
     public static int f(int[] a) {
-
-        return 5;
+        int sumBeforePoe = 0;
+        int sumAfterpoe = 0;
+        int poe = -1;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < i; j++) {
+                sumBeforePoe = sumBeforePoe + a[j];
+            }
+            for (int k = i + 1; k < a.length; k++) {
+                sumAfterpoe = sumAfterpoe + a[k];
+            }
+            if (sumBeforePoe == sumAfterpoe) {
+                poe = i;
+            }
+            sumBeforePoe = 0;
+            sumAfterpoe = 0;
+        }
+        return poe;
     }
 
     public static void main(String[] args) {
-        int[] arr = { 1, 8, 3, 7, 10, 2 };
+        int[] arr = { 1, 2, 3 };
         System.out.println(f(arr));
-
     }
 }
