db2fe9c3092238a18b040d9fb18adfd575424f74
Anil Khatiwada <anilkhatiwada@gmail.com>
2022-05-10 12:48:20 +0545

    QN1 added

--- src/QN1.java
+++ src/QN1.java
@@ -1,9 +1,26 @@
 public class QN1 {
-    static int Center(int arr[]){
-         
+  static int Center(int[] arr) {
+    int lengthOfarray = arr.length;
+    int indexOfMiddleElement = (int) (lengthOfarray / 2);
+    int counter = 0;
+    if (lengthOfarray % 2 == 0) {
+      return 0;
+    } else {
+      for (int i = 0; i < lengthOfarray; i++) {
+        if (arr[i] <= arr[indexOfMiddleElement] && i != indexOfMiddleElement) {
+          counter++;
+        }
+      }
     }
-    public static void main(String[] args) {
-      int data[] = {1,2,3,4,5};
-     System.out.println(data.length);
-    } 
+    if (counter == 0) {
+      return 1;
+    } else {
+      return 0;
+    }
   }
+
+  public static void main(String[] args) {
+    int[] data = {10};
+    System.out.println(Center(data));
+  }
+}
