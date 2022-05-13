14ab3979c37386745d0888debde3cd610de692b9
Anil Khatiwada <anilkhatiwada@gmail.com>
2022-05-13 18:49:08 +0545

    push

---
+++ src/SequencedArray.java
@@ -0,0 +1,23 @@
+public class SequencedArray {
+    public static int isSequencedArray(int[] a, int m, int n) {
+
+        if (a[0] != m || a[a.length - 1] != n) {
+            System.out.println("start end");
+            return 0;
+        }
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] + 1 == a[i + 1] || a[i] == a[i + 1]) {
+                continue;
+            } else {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        System.out.println(isSequencedArray(arr, 1, 5));
+    }
+
+}
